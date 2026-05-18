031cee08aa581de5c71ddad97c091da502b60887
Ryan <ryandam1473@googlemail.com>
2026-05-18 22:14:55 +0100

    Add .env vars, update docker network

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_APP_URL: string;
+  // Add any other custom variables here as your project grows
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
--- src/pages/games/GamesHomeView.vue
+++ src/pages/games/GamesHomeView.vue
@@ -1,3 +1,22 @@
+<script setup lang="ts">
+import { onMounted } from "vue";
+
+async function getData() {
+  const response = await fetch(import.meta.env.VITE_APP_URL);
+
+  if (!response.ok) {
+    console.log("womp womp");
+  }
+
+  const result = await response.json();
+  console.log(result);
+}
+
+onMounted(() => {
+  getData();
+});
+</script>
+
 <template>
   <div>test</div>
 </template>
